8b1f617b2497f36415b8dd607ca00cdf4b0015da
OnikuSpedu <srakibnyc1@gmail.com>
2020-10-25 15:07:11 -0400

    Created and tested ArrayOps's largest method.

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,5 +9,13 @@ public class ArrayOps {
         }
         return sum;
     }
-    
+    public static int largest(int[]arr) {
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -5,6 +5,6 @@ public class Tester {
     public static void main(String[] args) {
         int[] arr1 = {0, 1, 2, 3, 6, 2, 3, 9};
         System.out.println(ArrayOps.sum(arr1));
-    }
-    
+        System.out.println(ArrayOps.largest(arr1));
+    }   
 }
