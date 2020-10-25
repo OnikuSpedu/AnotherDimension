57563b6951ebf08d5b1b3833341b98843c6bc76b
OnikuSpedu <srakibnyc1@gmail.com>
2020-10-25 16:45:47 -0400

    Made tests print nicer.

--- Tester.java
+++ Tester.java
@@ -26,22 +26,39 @@ public class Tester {
             {2, 4, 2},
             {2, 2, 2},
         };
+        System.out.println("Sum (26 Expected):");
         System.out.println(ArrayOps.sum(arr1)); //26
+        System.out.println("Largest (9 Expected):");
         System.out.println(ArrayOps.largest(arr1)); //9
+        System.out.println("sumRows ([26, 12, 60] Expected):");
         System.out.println(Arrays.toString(ArrayOps.sumRows(matrix0))); // [26, 12, 60]
+        System.out.println("largestInRows ([9, 3, 23] Expected):");
         System.out.println(Arrays.toString(ArrayOps.largestInRows(matrix0))); // [9, 3, 23]
+        System.out.println("Sum (98 Expected):");
         System.out.println(ArrayOps.sum(matrix0)); //98
+        System.out.println("sumCols ([10, 25, 26, 11, 7, 5, 5, 9] Expected):");
         System.out.println(Arrays.toString(ArrayOps.sumCols(matrix0))); // [10, 25, 26, 11, 7, 5, 5, 9]
+        System.out.println("isRowMagic (false Expected):");
         System.out.println(ArrayOps.isRowMagic(matrix0)); //false
+        System.out.println("isRowMagic (true Expected):");
         System.out.println(ArrayOps.isRowMagic(matrix1)); //true
+        System.out.println("isRowMagic (false Expected):");
         System.out.println(ArrayOps.isRowMagic(matrix2)); //false
+        System.out.println("isColMagic (false Expected):");
         System.out.println(ArrayOps.isColMagic(matrix0)); //false
+        System.out.println("isColMagic (false Expected):");
         System.out.println(ArrayOps.isColMagic(matrix1)); //false
+        System.out.println("isColMagic (true Expected):");
         System.out.println(ArrayOps.isColMagic(matrix2)); //true
-        System.out.println(ArrayOps.isLocationMagic(matrix0, 1, 1));
-        System.out.println(ArrayOps.isLocationMagic(matrix1, 1, 2));
-        System.out.println(ArrayOps.isLocationMagic(matrix2, 2, 2));
-        System.out.println(ArrayOps.isLocationMagic(matrix3, 0, 1));
-        System.out.println(ArrayOps.isLocationMagic(matrix3, 1, 1));
+        System.out.println("isLocationMagic (false Expected):");
+        System.out.println(ArrayOps.isLocationMagic(matrix0, 1, 1)); //false
+        System.out.println("isLocationMagic (false Expected):");
+        System.out.println(ArrayOps.isLocationMagic(matrix1, 1, 2)); //false
+        System.out.println("isLocationMagic (false Expected):");
+        System.out.println(ArrayOps.isLocationMagic(matrix2, 2, 2)); //false
+        System.out.println("isLocationMagic (false Expected):");
+        System.out.println(ArrayOps.isLocationMagic(matrix3, 0, 1)); //false
+        System.out.println("isLocationMagic (true Expected):");
+        System.out.println(ArrayOps.isLocationMagic(matrix3, 1, 1)); //true
     }   
 }
