9956c576648e6e856be1e3226cdcffc25b50f50c
OnikuSpedu <srakibnyc1@gmail.com>
2020-10-25 15:04:44 -0400

    Created and tested ArrayOps's sum method.

---
+++ Tester.java
@@ -0,0 +1,10 @@
+/**
+ * Tester
+ */
+public class Tester {
+    public static void main(String[] args) {
+        int[] arr1 = {0, 1, 2, 3, 6, 2, 3, 9};
+        System.out.println(ArrayOps.sum(arr1));
+    }
+    
+}
